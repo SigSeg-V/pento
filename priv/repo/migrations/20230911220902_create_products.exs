7c6faf95cb1a37162f3ee618f5c0b677f1c6a636
Charlie <30825964+Vxlnce@users.noreply.github.com>
2023-09-12 19:16:42 +1000

    fix tests, added migration

--- priv/repo/migrations/20230911220902_create_products.exs
+++ priv/repo/migrations/20230911220902_create_products.exs
@@ -4,8 +4,13 @@ defmodule Pento.Repo.Migrations.CreateProducts do
   def change do
     create table(:products) do
       add :name, :string
+      add :description, :string
+      add :unit_price, :float
+      add :sku, :integer
 
       timestamps()
     end
+
+    create unique_index(:products, [:sku])
   end
 end
